085594cfd788ec23c89e9fec840fc89b795c9e9d
mykola-telychko <75166107+mykola-telychko@users.noreply.github.com>
2023-09-20 02:20:30 +0300

    name generator
    generate unique name / 500 000

--- kivi-db/index.js
+++ kivi-db/index.js
@@ -1,4 +1,6 @@
-
+const fs = require('fs');
+const path = require("path");
+// const {NAMES } = require('./config.js');
 
 // BUILD DATA GENERATOR FOR FILL KEY-VALUE DB
 const COUNTRY_CODE = {
@@ -8,10 +10,66 @@ const COUNTRY_CODE = {
             "Brazil": 55, "India": 91, "China": 86
 }
 
-const NAMES =  ['John', 'Jane', 'Michael', 'Emily', 'William', 'Olivia', 'James', 'Sophia',
-'Smith', 'Johnson', 'Brown', 'Williams', 'Jones', 'Miller', 'Davis', 'Garcia'];
+// const NAMES =  ['John', 'Jane', 'Michael', 'Emily', 'William', 'Olivia', 'James', 'Sophia',
+// 'Smith', 'Johnson', 'Brown', 'Williams', 'Jones', 'Miller', 'Davis', 'Garcia'];
+const NAMES = [
+    'Ivan',       'Petrov',     'Maria',        'Sidorova',   'Vasyl',
+    'Kovalenko',  'Olena',      'Lysenko',      'Andrii',     'Kozlov',
+    'Nataliia',   'Moroz',      'Mykhailo',     'Pavlenko',   'Sofia',
+    'Shevchenko', 'Yurii',      'Zaitsiv',      'Tetiana',    'Kravchenko',
+    'Oleh',       'Honchar',    'Iryna',        'Kuzmenko',   'Serhii',
+    'Boiko',      'Anna',       'Doroshenko',   'Dmytro',     'Hryhorenko',
+    'Liudmyla',   'Tkachenko',  'Roman',        'Savchenko',  'Kateryna',
+    'Levchenko',  'Vitalii',    'Rudenko',      'Yevhen',     'Melnyk',
+    'Olha',       'Fedorenko',  'Pavlo',        'Shevtsov',   'Halyna',
+    'Mazur',      'Artem',      'Korol',        'Larysa',     'Myronenko',
+    'Ihor',       'Shapoval',   'Tetiana',      'Polyakova',  'Ruslan',
+    'Serhiienko', 'Alla',       'Kotsiubynska', 'Yaroslav',   'Onopko',
+    'Lidiia',     'Bondarenko', 'Volodymyr',    'Frolov',     'Nina',
+    'Hrytsenko',  'Andrii',     'Panchenko',    'Oksana',     'Tereshchenko',
+    'Anatolii',   'Havryliuk',  'Maryna',       'Martynenko', 'Serhii',
+    'Lys',        'Tamara',     'Popova',       'Vladyslav',  'Bilous',
+    'Liubov',     'Kostenko',   'Viktor',       'Semenov',    'Nina',
+    'Bilenka',    'Oleksii',    'Riabokon',
+  'Hans', 'Müller', 'Anna', 'Schmidt',
+  'Michael', 'Schneider', 'Maria', 'Fischer',
+  'Thomas', 'Weber', 'Andrea', 'Meyer',
+  'Martin', 'Wagner', 'Susanne', 'Becker',
+  'Christian', 'Schulz',  'Ursula', 'Hoffmann',
+  'Stefan', 'Schäfer', 'Petra', 'Koch', 'Sebastian', 'Bauer',
+  'Monika', 'Richter', 'Markus', 'Klein',
+  'Sabine', 'Wolf', 'Patrick', 'Neumann', 'Silke', 'Schwarz',
+  'Alexander', 'Zimmermann', 'Kathrin', 'Braun', 'Daniel', 'Krüger',
+  'Melanie', 'Hofmann', 'Jürgen', 'Schmitt', 'Barbara', 'Werner',
+  'Tobias', 'Lange', 'Anja', 'Schmid',
+  'Nico', 'Krause', 'Tanja', 'Vogel',
+  'Oliver', 'Stein', 'Janine', 'Otto',
+  'Max', 'Günther', 'Jasmin', 'Berger',
+  'Sven', 'Arnold', 'Laura', 'Peters',
+  'Florian', 'Huber', 'Nina', 'Walter',
+  'Juan',      'Garcia',    'Maria',   'Rodriguez',
+  'Manuel',    'Martinez',  'Laura',   'Fernandez',
+  'Antonio',   'Lopez',     'Isabel',  'Sanchez',  
+  'Francisco', 'Torres',    'Ana',     'Perez',    
+  'David',     'Ramirez',   'Sofia',   'Gonzalez', 
+  'Pedro',     'Diaz',      'Luisa',   'Romero',   
+  'Miguel',    'Castro',    'Elena',   'Ruiz',     
+  'Rafael',    'Hernandez', 'Carmen',  'Navarro',
+  'Diego',     'Jimenez',   'Natalia', 'Morales',
+  'Carlos',    'Ortega',    'Raquel',  'Vega',
+  'Javier',    'Martin',    'Beatriz', 'Guerrero',
+  'Jose',      'Morales',   'Adriana', 'Medina',
+  'Roberto',   'Vargas',    'Lourdes', 'Leon',
+  'Gabriel',   'Soto',      'Silvia',  'Rios',
+  'John', 'Jane', 'Michael', 'Emily', 'William', 'Olivia', 'James', 'Sophia',
+  'Smith', 'Johnson', 'Brown', 'Williams', 'Jones', 'Miller', 'Davis', 'Garcia',
+  'Pablo',     'Castro',    'Marta',   'Navarro'
+];
 
-const QTY = 40;
+// const NAMES = cnf("NAMES");
+
+const QTY = 500000; // max: 3 000 000 // optimal: 500000
+const STORAGE = 'storage.txt';
 
 function generateRanKit(length) {
     if (length <= 0)  throw new Error("Length must be greater than 0");
@@ -60,10 +118,123 @@ function buildTable(keys, values){
     return tableData;
 }
 
-let nam = genRandNames( QTY, NAMES); 
-let num = genTelNums( QTY);
+let peoples = genRandNames( QTY, NAMES); 
+let telnum = genTelNums( QTY);
 
-  console.table( buildTable(nam, num) );
-  
+  // console.table( buildTable(peoples, telnum) );
 
-// console.log(  generateRanKit('sdfsd', 3));
+  // console.log(  JSON.stringify({nam, num})); // { nam:[], num:[] }
+  // console.log( nam.length, nam, "\n",
+  //                num.length, num );
+
+  // функція для переливання з массива в массив на вказану кількість елементів 
+  function moveElements(sourceArray, targetArray, count) {
+    if (!Array.isArray(sourceArray) || !Array.isArray(targetArray) || count < 0) {
+      throw new Error('Параметри некоректні');
+    }
+    const elementsToMove = sourceArray.slice(0, count); // Копіюємо елементи, а не видаляємо їх
+    const newTargetArray = [...targetArray, ...elementsToMove]; // Створюємо новий масив, що включає переміщені елементи
+    return newTargetArray;
+  }
+
+  function generateAndPushUniqueElements(qty, targetArray, spareMilElem) {
+        // spareMilElem -> targetArray (Nu el)
+        let Nu =  qty - targetArray.length;
+        if ( Nu > 0 ) {
+            let finArr = moveElements(spareMilElem, targetArray, Nu);
+            // console.log( Nu, finArr );
+            return finArr;
+        } else {
+            return targetArray;
+        }
+  }
+
+
+
+  const millionUniqueNames = generateUniqueNamesArray(1000000);
+  let unames = uniqueNameArray(peoples);
+  let uNamArr =  generateAndPushUniqueElements(QTY , unames, millionUniqueNames);
+    // console.log( appi ); 
+  let dataForDb =  concatenateArraysAlternately(uNamArr, telnum).join('|');
+  insertToDB(dataForDb); // write to db
+
+
+
+
+function insertToDB(database) {
+
+  let validData = true;
+  // console.log('validData', validData);
+  if ( validData ) {
+      let txt = database;
+
+      fs.writeFile(STORAGE, txt,
+          {encoding: "utf8", flag: "w", mode: 0o666},
+          (err) => {
+              if (err) console.log(err);
+              else {
+                  console.log("The written has the following contents:");
+              }
+          }
+      );
+  }
+}
+
+// insertToDB(JSON.stringify({...nam, ...num}));
+
+// HELPERS 
+function uniqueNameArray(arr) {
+  const uniqueArray = []; const seenValues = {};
+
+  for (const value of arr) {
+    if (!seenValues[value]) {
+      uniqueArray.push(value);
+      seenValues[value] = true;
+    }
+  }
+  return uniqueArray;
+}
+
+///////
+function generateRandomName() {
+  const length = 6; // Довжина імені
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'; // Символи, які можуть бути в імені
+  let name = '';
+
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    name += characters.charAt(randomIndex);
+  }
+
+  return name;
+}
+
+// use for custom hash fn
+function generateUniqueNamesArray(count) {
+  const uniqueNames = new Set();
+
+  while (uniqueNames.size < count) {
+    const name = generateRandomName();
+    uniqueNames.add(name);
+  }
+
+  return Array.from(uniqueNames);
+}
+
+function concatenateArraysAlternately(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    throw new Error('Масиви мають різну довжину');
+  }
+
+  const result = [];
+
+  for (let i = 0; i < arr1.length; i++) {
+    result.push(arr1[i]);
+    result.push(arr2[i]);
+  }
+
+  return result;
+}
+
+
+
