4953939eaac236511eb984321e59cace53dbd608
mykola-telychko <75166107+mykola-telychko@users.noreply.github.com>
2023-09-23 17:02:00 +0300

    first query and problem reading and parsing

--- kivi-db/drawwork.js
+++ kivi-db/drawwork.js
@@ -1,72 +1,124 @@
-function splitArrayElements(array) {
-    const result = [];
-    
-    for (let i = 0; i < array.length; i++) {
-      const words = array[i].split(' ');
-      result.push(...words);
-    }
-    
-    return result;
-  }
-  
-  // Приклад використання:
-  const ukrainianNames = [
-    'Ivan Petrov',
-    'Maria Sidorova',
-    'Vasyl Kovalenko',
-    'Olena Lysenko',
-    'Andrii Kozlov',
-    'Nataliia Moroz',
-    'Mykhailo Pavlenko',
-    'Sofia Shevchenko',
-    'Yurii Zaitsiv',
-    'Tetiana Kravchenko',
-    'Oleh Honchar',
-    'Iryna Kuzmenko',
-    'Serhii Boiko',
-    'Anna Doroshenko',
-    'Dmytro Hryhorenko',
-    'Liudmyla Tkachenko',
-    'Roman Savchenko',
-    'Kateryna Levchenko',
-    'Vitalii Rudenko',
-    'Yevhen Melnyk',
-    'Olha Fedorenko',
-    'Pavlo Shevtsov',
-    'Halyna Mazur',
-    'Artem Korol',
-    'Larysa Myronenko',
-    'Ihor Shapoval',
-    'Tetiana Polyakova',
-    'Ruslan Serhiienko',
-    'Alla Kotsiubynska',
-    'Yaroslav Onopko',
-    'Lidiia Bondarenko',
-    'Volodymyr Frolov',
-    'Nina Hrytsenko',
-    'Andrii Panchenko',
-    'Oksana Tereshchenko',
-    'Anatolii Havryliuk',
-    'Maryna Martynenko',
-    'Serhii Lys',
-    'Tamara Popova',
-    'Vladyslav Bilous',
-    'Liubov Kostenko',
-    'Viktor Semenov',
-    'Nina Bilenka',
-    'Oleksii Riabokon',
-    // Додайте інші імена та прізвища тут
-  ];
-  
+#!/usr/bin/env node
+const fs = require('fs');
+const path = require("path");
+const dbFilePath = path.join(__dirname, 'storage.txt');
 
-  
-  const outputArray = splitArrayElements(ukrainianNames);
-  console.log(outputArray);
-  function splitArrayElements(array) {
-    const result = [];  
-    for (let i = 0; i < array.length; i++) {
-      const words = array[i].split(' ');
-      result.push(...words);
-    }
-    return result;
+const config = require('./config.json');
+const COUNTRY_CODE = config.countryCode;
+
+
+// Check if at least one command-line argument is provided
+if (process.argv.length < 3) {
+  console.error('node cli_example.js <message>');
+  process.exit(1); // Exit the script with a non-zero status code to indicate an error
+}
+
+// Access the command-line argument at index 2 (index 0 and 1 are reserved for node and script name)
+const message1 = process.argv[2];
+const message2 = process.argv[3];
+const message3 = process.argv[4];
+// remove first two elms 
+const [, , ... queryArray ] = process.argv;
+
+let dbTXT;
+
+
+
+// console.log('KV:', message1, message2, message3);
+console.log('KV:', queryArray);
+// handeQuery(queryArray);
+
+// get region telephone num 
+function handeQuery(query){
+  COUNTRY_CODE
+
+  switch (query[0]) {
+    case "READ":
+      // let per =  readDbJSON((database) => {  return database;} );
+
+      // get country code NUM on countryName 
+      // console.log("It.", COUNTRY_CODE[capitalizeFirstLetter(query[query.length - 1])] );
+      // console.log("db.", per)
+      return COUNTRY_CODE[capitalizeFirstLetter(query[query.length - 1])];
+      // read from db 
+
+      break;
+    case "CREATE":
+      console.log("It's Tuesday.");
+      break;
+    case "DELETE":
+      console.log("It's Wednesday.");
+      break;
+    case "UPDATE":
+      console.log("It's almost the weekend!");
+      break;
+    default:
+      console.error("Error!");
   }
+}
+// node .\drawwork.js SELECT FROM 'canada'
+
+
+function readDbJSON(callback) {
+  fs.readFile(dbFilePath, 'utf8', (err, data) => {
+       if (err) {
+           console.error('Error reading database:', err);
+           return;
+       }
+
+       try {
+          //  dbJSON = JSON.parse(data); callback(dbJSON);
+          // handeQuery(queryArray);
+
+          //  dbTXT = parseTxt(data); 
+          // https://github.com/mykola-telychko/assistant-js
+           dbTXT = parseTxt(data); 
+
+           
+           callback(dbTXT);// get variable outer ,
+
+          //  console.log('txt::', dbTXT);
+       } catch (parseError) { console.error('Error parsing JSON:', parseError); }
+       // console.log('data::', dbJSON);
+  });
+   return dbTXT;
+}
+// readDbJSON((database) => { insertTxtDB(database); return database;} );
+readDbJSON((database) => { console.log(database); return database;} );
+
+function parseTxt(txt){
+  let arr = txt.split("|");
+  const [evenArr, oddArr] = explodeArray(arr);
+
+ let code = handeQuery(queryArray);
+
+
+  const matchingElements = oddArr.filter(item => item.startsWith("+" + code));
+
+  console.log('code', matchingElements);
+
+  return ;
+}
+
+function explodeArray(srcArr) {
+  const evenArr = [];
+  const oddArr = [];
+
+  for (let i = 0; i < srcArr.length; i++) {
+    if (i % 2 === 0) {
+      evenArr.push(srcArr[i]);
+    } else {
+      oddArr.push(srcArr[i]);
+    }
+  }
+  return [evenArr, oddArr];
+}
+
+// const [evenArr, oddArr] = explodeArray(arr0);
+// const [evenArr, oddArr] = explodeArray(arrNonUniqueElements);
+// console.log(oddArr);
+
+// HELPERS 
+function capitalizeFirstLetter(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
---
+++ kivi-db/generate.js
@@ -0,0 +1,157 @@
+const fs = require('fs');
+const path = require("path");
+const config = require('./config.json');
+
+// BUILD DATA GENERATOR FOR FILL KEY-VALUE DB
+const COUNTRY_CODE = config.countryCode;
+const NAMES = config.telNumbers;
+const QTY = 30000; // max: 3 000 000 // optimal: 500000
+const STORAGE = 'storage.txt';
+// console.log(config);
+
+
+function generateRanKit(length) {
+    if (length <= 0)  throw new Error("Length must be greater than 0");
+  
+    const min = Math.pow(10, length - 1);
+    const max = Math.pow(10, length) - 1;
+  
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function genRandNames(quantity, elements) {
+    const randomStrings = []; const elementsCount = elements.length;
+  
+    for (let i = 0; i < quantity; i++) {
+          let randomString = '';
+          for (let j = 0; j < 3; j++) {
+            const randomIndex = Math.floor(Math.random() * elementsCount);
+            randomString += elements[randomIndex];
+          }
+          randomStrings.push(randomString);
+    } 
+    return randomStrings;
+}
+
+// build full telephone num 
+function genTelNums(quantity) {
+    const CODE = Object.values(COUNTRY_CODE);
+    let finArr = [];
+
+    for ( let i = 0; i < quantity; i++ ) {
+         finArr.push('+' + getRandElFromArray(CODE) + generateRanKit(10));
+    }
+    return finArr;
+}
+
+function generateAndPushUniqueElements(qty, targetArray, spareMilElem) {
+      // spareMilElem -> targetArray (Nu el)
+      let insufficientNum =  qty - targetArray.length;
+      if ( insufficientNum > 0 ) {
+          let finArr = draggingElements(spareMilElem, targetArray, insufficientNum);
+          return finArr;
+      } else { return targetArray; }
+}
+
+function insertToDB(database) {
+  let validData = true;
+  if ( validData ) {
+      let txt = database;
+
+      fs.writeFile(STORAGE, txt, {encoding: "utf8", flag: "w", mode: 0o666},
+          (err) => {
+              if (err) console.log(err);
+              else {
+                  console.log("The written has the following contents:");
+              }
+          }
+      );
+  }
+}
+
+// constructor - start 
+let peoples = genRandNames(QTY, NAMES); 
+let telnum = genTelNums(QTY);
+
+const millionUniqueNames = generateUniqueNamesArray(1000000);
+let unames = uniqueNameArray(peoples);
+let uNamArr =  generateAndPushUniqueElements(QTY , unames, millionUniqueNames);
+  // console.log( appi ); 
+let dataForDb =  concatenateArraysAlternately(uNamArr, telnum).join('|');
+insertToDB(dataForDb); // write to db
+// console.table( buildTable(peoples, telnum) );
+// constructor - end 
+
+
+// HELPERS -- START
+function uniqueNameArray(arr) {
+    const uniqueArray = []; const seenValues = {};
+
+    for (const value of arr) {
+        if (!seenValues[value]) {
+          uniqueArray.push(value); seenValues[value] = true;
+        }
+    }
+    return uniqueArray;
+}
+
+function generateRandomName() {
+    const length = 8; // name len
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'; 
+    let name = '';
+
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * characters.length);
+        name += characters.charAt(randomIndex);
+    }
+    return name;
+}
+
+function buildTable(keys, values){
+    const tableData = [];
+    for (let i = 0; i < Math.max(keys.length, values.length); i++) {
+      tableData.push({ Column1: keys[i], Column2: values[i] });
+    }
+    return tableData;
+}
+
+// use for custom hash fn
+function generateUniqueNamesArray(count) {
+    const uniqueNames = new Set();
+
+    while (uniqueNames.size < count) {
+      const name = generateRandomName();
+      uniqueNames.add(name);
+    }
+    return Array.from(uniqueNames);
+}
+
+function concatenateArraysAlternately(arr1, arr2) {
+    if (arr1.length !== arr2.length) throw new Error('diff arr len');
+   
+    const result = [];
+    for (let i = 0; i < arr1.length; i++) {
+      result.push(arr1[i]); result.push(arr2[i]);
+    }
+    return result;
+}
+
+// getting random element from array 
+function getRandElFromArray(arr) {
+  const randomIndex = Math.floor(Math.random() * arr.length);
+  return arr[randomIndex];
+}
+
+// draggingElements from arr1 to arr2 - specified qty 
+function draggingElements(sourceArray, targetArray, count) {
+    if (!Array.isArray(sourceArray) || !Array.isArray(targetArray) || count < 0) {
+      throw new Error('Параметри некоректні');
+    }
+    const elementsToMove = sourceArray.slice(0, count); // Копіюємо елементи, а не видаляємо їх
+    const newTargetArray = [...targetArray, ...elementsToMove]; 
+    return newTargetArray;
+}
+// HELPERS -- START
+
+
+
